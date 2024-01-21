1443a414522f90e2355bf671f15ebb2e4a6f54fa
Chanaka Abeysinghe <chanakasan@gmail.com>
2024-01-21 23:35:16 +0530

    rev

--- bin/create-new-vite-react.sh
+++ bin/create-new-vite-react.sh
@@ -2,6 +2,11 @@ create_new_vite_react() {
   app_name="$1"
   echo " create_new_vite_react"
   echo " => app_name: $app_name"
+
+  if [ -z $app_name ]; then
+    echo "app_name required"
+    exit
+  fi
   
   npm create vite@latest $app_name -- --template react-ts
   cd $app_name
