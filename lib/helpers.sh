089a859c5a4a61a2a2ab2a04446c9ea11d336213
Chanaka Abeysinghe <chanakasan@gmail.com>
2024-01-30 22:49:15 +0530

    rev

--- lib/helpers.sh
+++ lib/helpers.sh
@@ -1,5 +1,5 @@
-source $nex_script_path/lib/helpers_1.sh
-source $nex_script_path/lib/helpers_2.sh
-source $nex_script_path/lib/helpers_3.sh
-source $nex_script_path/lib/helpers_4.sh
-source $nex_script_path/lib/helpers_5.sh
+source $devscript_path/lib/helpers_1.sh
+source $devscript_path/lib/helpers_2.sh
+source $devscript_path/lib/helpers_3.sh
+source $devscript_path/lib/helpers_4.sh
+source $devscript_path/lib/helpers_5.sh
