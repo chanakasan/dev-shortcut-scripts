6142160175505cf5c9187551169cdb9e9e21611a
Chanaka Abeysinghe <chanakasan@gmail.com>
2024-02-01 10:55:39 +0530

    rev

--- src/install-formik.sh
+++ src/install-formik.sh
@@ -19,22 +19,13 @@ install_packages() {
   add_to_commit package.json package-lock.json
 }
 
-copy_files() {
-  local base="$wd/modules"
-  local name="formik"
-  local dest_path=$base/$name
-  run_cmd mkdir -p $base
-  run_cmd npx degit --mode=git NexParts/module-formik-1 -- $dest_path
-  add_to_commit $dest_path
-}
-
 main() {
   local wd="$1"
   local flags="$2"
   local wd_ans=""
   start_and_validate
   install_packages
-  copy_files
+  copy_module "formik"
   finish_and_commit
 }
 
