26118756351180d93ac2753512bec8c758b2e820
Chanaka Abeysinghe <chanakasan@gmail.com>
2024-02-01 12:33:17 +0530

    rev

--- src/install-testing-library.sh
+++ src/install-testing-library.sh
@@ -4,27 +4,12 @@ set -e
 
 source $devscript_helpers
 
-start_and_validate() {
-  print_line "=> Script: install testing-library"
-  validate_wd_git
-}
-
-finish_and_commit() {
-  commit_changes "script: install testing-library"
-  print_line "=> done"
-}
-
-install_packages() {
-  run_npm_install -D @testing-library/react @testing-library/jest-dom @testing-library/user-event 
-  add_to_commit package.json package-lock.json
-}
-
 main() {
   local wd="$1"
-  local flags="$2"
   local wd_ans=""
+  local script_name="install testing-library"
   start_and_validate
-  install_packages
+  install_packages -D @testing-library/react @testing-library/jest-dom @testing-library/user-event 
   finish_and_commit
 }
 
