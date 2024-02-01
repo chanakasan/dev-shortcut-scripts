1d31ec10fa7fa9c08fb82c2712d2508ae68a4af5
Chanaka Abeysinghe <chanakasan@gmail.com>
2024-02-01 12:20:08 +0530

    rev

--- src/install-tailwind-cli.sh
+++ src/install-tailwind-cli.sh
@@ -5,27 +5,12 @@ set -e
 
 source $devscript_helpers
 
-start_and_validate() {
-  print_line "=> Script: install tailwind-cli"
-  validate_wd_git
-}
-
-finish_and_commit() {
-  commit_changes "script: install tailwind-cli"
-  print_line "=> done"
-}
-
-install_packages() {
-  run_npm_install -D tailwindcss
-  add_to_commit package.json package-lock.json
-}
-
 main() {
   local wd="$1"
   local wd_ans=""
-  local flags="$2"
+  local script_name="install tailwind_cli"
   start_and_validate
-  install_packages
+  install_packages -D tailwindcss
   copy_module "tailwind_cli"
   finish_and_commit
 }
