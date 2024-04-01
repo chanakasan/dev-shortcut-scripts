511c0d5ca15b5d05b0281800319a11d62802b916
Chanaka Abeysinghe <chanakasan@gmail.com>
2024-04-01 09:06:52 +0530

    rev

---
+++ src/install/hello.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+
+source $devscript_path/support/install/index.sh
+
+main() {
+  local wdir="$1"
+  local wdir_answer=""
+  local script_name="install/hello"
+  echo script_name: $script_name
+  echo done
+  echo
+}
+
+main "$@"
